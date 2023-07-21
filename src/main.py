7082b4909a3782a25b1896ac9026d6b11c04250d
Brian <Brian@blackverse.co.kr>
2023-07-22 04:45:49 +0900

    svc setting

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import mod.discordbot
 import os
 from dotenv import load_dotenv
 
-SERVICE_STATE = "dev"
+SERVICE_STATE = "svc"
 
 if __name__ == "__main__":
     load_dotenv(f"./token_{SERVICE_STATE}.env") # load all the variables from the env file
